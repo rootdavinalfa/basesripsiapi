4089ed1477c7b8da5da74048404b146157475315
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2021-03-24 20:38:02 +0700

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
